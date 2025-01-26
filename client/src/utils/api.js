3b978d9c49536ea05d1e18b8eecba7883e33ff37
YASH <yashsanjay.k21@iiits.in>
2025-01-26 14:51:30 +0530

    more change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Updated baseURL
+  baseURL: "https://borneo-assignment.onrender.com/api", // Updated baseURL
   headers: {
     "Content-Type": "application/json",
   },
